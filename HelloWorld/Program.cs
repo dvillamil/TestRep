a12fb1bfdc1918cf42560235a5c92d1f67f744ff
dvillamil <diany.villamil@gmail.com>
2018-08-07 16:33:23 -0500

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine(createMessage());
-            Console.WriteLine("Hello Diana Maria Villamil");            
+            Console.WriteLine("Hello Diana Maria");            
         }
         public static string createMessage()
         {
